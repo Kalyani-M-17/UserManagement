784ddcbbaf66991a29a1a45ec2e297ba9be0b5b5
Kalyani <17kalyani2001@gmail.com>
2024-06-23 01:19:21 +0530

    setup express

--- Backend/server.js
+++ Backend/server.js
@@ -1,1 +1,19 @@
-console.log("HI");
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+app.use(bodyParser.json());
+
+app.get("/", (req,res) =>{
+    console.log("res");
+
+    res.send("Working.....");
+});
+
+app.listen(5500, () => {
+    console.log("Server Started.....");
+});
+
+
+
+
