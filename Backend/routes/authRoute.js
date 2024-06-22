bd91465f958527227fb86325a3faebe77d68a380
Kalyani <17kalyani2001@gmail.com>
2024-06-23 02:21:29 +0530

    setup routes

---
+++ Backend/routes/authRoute.js
@@ -0,0 +1,14 @@
+const Router = require("express").Router;
+
+const router = Router();
+
+router.post("/login", (req,res) => {
+    res.send("Login...");
+});
+
+router.post("/register", (req,res) => {
+    res.send("Register...");
+});
+
+module.exports = router;
+
--- Backend/server.js
+++ Backend/server.js
@@ -1,14 +1,17 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const authRoute = require("./routes/authRoute");
 
 const app = express();
 app.use(bodyParser.json());
 
-app.get("/", (req,res) =>{
-    console.log("res");
+// app.get("/", (req,res) =>{
+//     console.log("res");
 
-    res.send("Working.....");
-});
+//     res.send("Working.....");
+// });
+
+app.use("/auth", authRoute);
 
 app.listen(5500, () => {
     console.log("Server Started.....");
