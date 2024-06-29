04ac3caf79ef6a5208717e5dbc86cf307a254711
Kalyani <17kalyani2001@gmail.com>
2024-06-29 12:02:16 +0530

    Hide menu items based on login status

--- Frontend/src/components/NavBar.jsx
+++ Frontend/src/components/NavBar.jsx
@@ -2,39 +2,46 @@ import { Link } from "react-router-dom";
 import { Button, Nav, NavItem } from "reactstrap";
 
 function NavBar() {
-    return (
-        <div className="p-4 bg-secondary text-white d-flex justify-content-between align-items-center">
-            <h4>MyApp</h4>
-            <Nav className="gap-4">
-                <NavItem>
-                    <Link className="text-white" to={"/"}>
-                    Home
-                    </Link>
-                </NavItem>
-                <NavItem>
-                    <Link className="text-white" to={"/register"}>
-                    Register
-                    </Link>
-                </NavItem>
-                <NavItem>
-                    <Link className="text-white" to={"/login"}>
-                    Login
-                    </Link>
-                </NavItem>
-                <NavItem>
-                    <Button
-                        onClick={() => {
-                            localStorage.removeItem("token");
-                            window.location.replace("/login");
-                        }}
-                    >
-                        Logout
-                    </Button>
-                </NavItem>
-            </Nav>
+  const token = localStorage.getItem("token");
 
-        </div>
-    );
+  return (
+    <div className="p-4 bg-secondary text-white d-flex justify-content-between align-items-center">
+      <h4>MyApp</h4>
+      <Nav className="gap-4">
+        <NavItem>
+          <Link className="text-white" to={"/"}>
+            Home
+          </Link>
+        </NavItem>
+        {!token && (
+          <NavItem>
+            <Link className="text-white" to={"/register"}>
+              Register
+            </Link>
+          </NavItem>
+        )}
+        {!token && (
+          <NavItem>
+            <Link className="text-white" to={"/login"}>
+              Login
+            </Link>
+          </NavItem>
+        )}
+        {!token && (
+          <NavItem>
+            <Button
+              onClick={() => {
+                localStorage.removeItem("token");
+                window.location.replace("/login");
+              }}
+            >
+              Logout
+            </Button>
+          </NavItem>
+        )}
+      </Nav>
+    </div>
+  );
 }
 
 export default NavBar;
