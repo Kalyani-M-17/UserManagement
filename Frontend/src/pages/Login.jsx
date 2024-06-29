151506c3d302f71d8df8cc6f4648bcb78dc5f2d5
Kalyani <17kalyani2001@gmail.com>
2024-06-29 14:54:37 +0530

    Show invalid credentials message

--- Frontend/src/pages/Login.jsx
+++ Frontend/src/pages/Login.jsx
@@ -1,73 +1,89 @@
 import { useState } from "react";
-import { Button, Card, CardBody, CardHeader, CardTitle, Form, FormGroup, Input, Label } from "reactstrap";
+import {
+  Button,
+  Card,
+  CardBody,
+  CardHeader,
+  CardTitle,
+  Form,
+  FormGroup,
+  Input,
+  Label,
+} from "reactstrap";
 import { axiosInstance } from "../utils/axios";
 
 function LoginPage() {
-    const [data, setData] = useState({
-        email: "",
-        password: "",
-    });
+  const [data, setData] = useState({
+    email: "",
+    password: "",
+  });
 
-    const handle = (e) => {
-        e.preventDefault();
-        axiosInstance.post("/auth/login", data).then((res) => {
-            console.log(res);
-            if(res.status == 200){  
-                alert("Logged In");
-                localStorage.setItem("token", res.data.token);
-                localStorage.setItem("email_verified", res.data.email_verified);
+  const handle = (e) => {
+    e.preventDefault();
+    axiosInstance
+      .post("/auth/login", data)
+      .then((res) => {
+        console.log(res);
+        if (res.status == 200) {
+          alert("Logged In");
+          localStorage.setItem("token", res.data.token);
+          localStorage.setItem("email_verified", res.data.email_verified);
 
-                if (res.data.email_verified == null) {
-                    window.location.replace("/verify");
-                } else {
-                    window.location.replace("/");
-                }
-            }
-            setData({
-                email: "",
-                password: "",
-            });
+          if (res.data.email_verified == null) {
+            window.location.replace("/verify");
+          } else {
+            window.location.replace("/");
+          }
+        }
+        setData({
+          email: "",
+          password: "",
         });
-    };
+      })
+      .catch((err) => {
+        console.log(err);
+        if (err.response.status === 400) {
+          alert("Invalid credentials");
+        }
+      });
+  };
 
-    return (
-        <div className="container m-5">
-            <Card>
-                <CardHeader> 
-                    <CardTitle tag="h5">Login</CardTitle>    
-                </CardHeader>
-                <CardBody>
-                    <Form onSubmit={handle}>
-                        <FormGroup>
-                            <Label>Email</Label>
-                            <Input
-                            required 
-                            type="email"
-                            value = {data.email}
-                            onChange={(e) => {
-                                setData({...data, email: e.target.value});
-                            }} 
-                            />
-                        </FormGroup>
-                        <FormGroup>
-                            <Label>Password</Label>
-                            <Input
-                            required 
-                            type="password"
-                            value = {data.password}
-                            onChange={(e) => {
-                                setData({...data, password: e.target.value});
-                            }} 
-                            />
-                        </FormGroup>
+  return (
+    <div className="container m-5">
+      <Card>
+        <CardHeader>
+          <CardTitle tag="h5">Login</CardTitle>
+        </CardHeader>
+        <CardBody>
+          <Form onSubmit={handle}>
+            <FormGroup>
+              <Label>Email</Label>
+              <Input
+                required
+                type="email"
+                value={data.email}
+                onChange={(e) => {
+                  setData({ ...data, email: e.target.value });
+                }}
+              />
+            </FormGroup>
+            <FormGroup>
+              <Label>Password</Label>
+              <Input
+                required
+                type="password"
+                value={data.password}
+                onChange={(e) => {
+                  setData({ ...data, password: e.target.value });
+                }}
+              />
+            </FormGroup>
 
-                        <Button>Login</Button>
-
-                    </Form>
-                </CardBody>
-            </Card>
-
-        </div>
-    )
+            <Button>Login</Button>
+          </Form>
+        </CardBody>
+      </Card>
+    </div>
+  );
 }
 export default LoginPage;
